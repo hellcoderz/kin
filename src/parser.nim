3ebc2953c625bda089af8fd7afe20f3018657088
vsagar200 <Vikrant_Sagar@cable.comcast.com>
2018-05-14 00:23:40 -0400

    adding some parser grammer

--- src/parser.nim
+++ src/parser.nim
@@ -1,7 +1,26 @@
-import ./kin1, tables, strutils, sequtils
+import ./kin1, tables, strutils, sequtils, pegs
 
-proc parse(program: string): Kobj =
-    return kfn(kplus, ki(1), ki(4))
+var grammar = peg"""
+    expr <- symbol (verb / adverb) expr / function / number verb expr / number / \s expr
+    function <- "{" expr "}"
+    symbol <- {[`]?[A-Za-z][A-Za-z0-9_]*}
+    number <- float / int
+    int <- {\d+}
+    float <- {\d+ '.' \d+}
+    verb <- { ["+-*%"] }
+    adverb <- { [":/\\'"] }
+"""
+
+var programs = @[
+    "34+45.5",
+    "45",
+    "{3+44}",
+    "mean: 3+4"
+]
+
+for program in programs:
+    discard program =~ grammar
+    echo matches
 
 if isMainModule:
     echo "Parsing Module for K Language implementation in Nim [v0.0.1]"
--- src/parser_test.nim
+++ src/parser_test.nim
@@ -1,5 +1,6 @@
 import ./parser, unittest, macros, sequtils, strutils
 
-suite "Katom test":
-    test "knumber(s) as input":
-        check 1 == 1
+var
+    p1 = "3+4"
+    p2 = "3+1 2 3"
+    p3 = "1 2 3+1 2 3"
