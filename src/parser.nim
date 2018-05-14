d447d6877d7089a9ee5346bba10c8f30512b743e
vsagar200 <Vikrant_Sagar@cable.comcast.com>
2018-05-14 02:15:51 -0400

    now using pcre in nIm and adding custom parser

--- src/parser.nim
+++ src/parser.nim
@@ -1,26 +1,26 @@
-import ./kin1, tables, strutils, sequtils, pegs
+import ./kin1, tables, strutils, sequtils, nre
 
-var grammar = peg"""
-    expr <- symbol (verb / adverb) expr / function / number verb expr / number / \s expr
-    function <- "{" expr "}"
-    symbol <- {[`]?[A-Za-z][A-Za-z0-9_]*}
-    number <- float / int
-    int <- {\d+}
-    float <- {\d+ '.' \d+}
-    verb <- { ["+-*%"] }
-    adverb <- { [":/\\'"] }
-"""
-
-var programs = @[
-    "34+45.5",
-    "45",
-    "{3+44}",
-    "mean: 3+4"
-]
-
-for program in programs:
-    discard program =~ grammar
-    echo matches
+var NUMBER*  = re"^(-?0w|0N|-?\d+\.\d*|-?\d*\.?\d+)"
+var HEXLIT*  = re"(^0x[a-zA-Z\d]+)"
+var BOOL*    = re"(?i:^[01]+b)"
+var NAME*    = re"(?i:^[a-z][a-z\d]*)"
+var SYMBOL*  = re"(?i:^`([a-z.][a-z0-9.]*)?)"
+var STRING*  = re""" ^"(\\.|[^"\\\r\n])*" """
+var VERB*    = re"^[+\-*%!&|<>=~,^#_$?@.]"
+var ASSIGN*  = re"^[+\-*%!&|<>=~,^#_$?@.]:"
+var IOVERB*  = re"^\d:"
+var ADVERB*  = re"^['\\\/]:?"
+var SEMI*    = re"^;"
+var COLON*   = re"^:"
+var VIEW*    = re"^::"
+var COND*    = re"^\$\["
+var DICT*    = re"(?i:^\[[a-z]+:)"
+var OPEN_B*  = re"^\["
+var OPEN_P*  = re"^\("
+var OPEN_C*  = re"^{"
+var CLOSE_B* = re"^\]"
+var CLOSE_P* = re"^\)"
+var CLOSE_C* = re"^}"
 
 if isMainModule:
     echo "Parsing Module for K Language implementation in Nim [v0.0.1]"
--- src/parser_test.nim
+++ src/parser_test.nim
@@ -1,6 +1,19 @@
-import ./parser, unittest, macros, sequtils, strutils
+import ./parser, unittest, macros, sequtils, strutils, nre, ./programs
 
-var
-    p1 = "3+4"
-    p2 = "3+1 2 3"
-    p3 = "1 2 3+1 2 3"
+suite "Testing Regexes":
+    test "NUMBER":
+        echo "56".find(NUMBER)
+        echo "564242".find(NUMBER)
+        echo "56.2423".find(NUMBER)
+        echo "-56".find(NUMBER)
+        echo "0N".find(NUMBER)
+        echo "-342.2342".find(NUMBER)
+        echo "-.3232".find(NUMBER)
+        echo "0w".find(NUMBER)
+
+    test "HEXLIT":
+        echo "0xadadsad".find(HEXLIT)
+
+    test "BOOL":
+        echo "010101b".find(BOOL)
+        echo "001011B".find(BOOL)
